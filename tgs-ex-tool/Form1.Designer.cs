37cc38036c5540086bb94ffaea36c10d24dbe0f6
YuTanaka <YRK00337@nifty.com>
2015-11-12 21:47:57 +0900

    VSプロジェクトを追加

---
+++ tgs-ex-tool/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace 試験登録
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textUID = new System.Windows.Forms.TextBox();
+            this.btnEntry = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "学籍番号";
+            // 
+            // textUID
+            // 
+            this.textUID.ImeMode = System.Windows.Forms.ImeMode.Alpha;
+            this.textUID.Location = new System.Drawing.Point(130, 10);
+            this.textUID.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.textUID.Name = "textUID";
+            this.textUID.Size = new System.Drawing.Size(208, 29);
+            this.textUID.TabIndex = 1;
+            // 
+            // btnEntry
+            // 
+            this.btnEntry.Location = new System.Drawing.Point(26, 54);
+            this.btnEntry.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.btnEntry.Name = "btnEntry";
+            this.btnEntry.Size = new System.Drawing.Size(315, 40);
+            this.btnEntry.TabIndex = 2;
+            this.btnEntry.Text = "試験出席登録";
+            this.btnEntry.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(362, 108);
+            this.Controls.Add(this.btnEntry);
+            this.Controls.Add(this.textUID);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Name = "Form1";
+            this.Text = "試験登録";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textUID;
+        private System.Windows.Forms.Button btnEntry;
+    }
+}
+

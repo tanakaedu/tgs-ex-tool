1e085b1b14b45d0f0b27df73bd1b519386f28de2
YuTanaka <YRK00337@nifty.com>
2015-11-13 00:42:47 +0900

    サーバー：情報表示

--- TgsExServer/TgsExServer/Form1.Designer.cs
+++ TgsExServer/TgsExServer/Form1.Designer.cs
@@ -28,12 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.ColumnCount = 6;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(326, 33);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(732, 376);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "試験サーバー";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+
+
     }
 }
 
--- TgsExServer/TgsExServer/Form1.cs
+++ TgsExServer/TgsExServer/Form1.cs
@@ -11,9 +11,149 @@ namespace TgsExServer
 {
     public partial class Form1 : Form
     {
+        /** テストデータ*/
+        string [,] testData = new string[,]{
+            {"21531000","123.123.123.123","0","x","path"},
+            {"21531999","124.124.124.124","1","x","path2"},
+            {"21531999","124.124.124.124","1","x","path2"},
+            {"21531999","124.124.124.124","1","x","path2"},
+            {"21531999","124.124.124.124","1","x","path2"},
+            {"21531999","124.124.124.124","1","x","path2"},
+            {"21531999","124.124.124.124","1","x","path2"},
+            {"21531999","124.124.124.124","1","x","path2"},
+            {"21531999","124.124.124.124","1","x","path2"},
+            {"21531999","124.124.124.124","1","x","path2"},
+            {"21531999","124.124.124.124","1","x","path2"},
+            {"21531999","124.124.124.124","1","x","path2"},
+            {"21531999","124.124.124.124","1","x","path2"},
+            {"21531999","124.124.124.124","1","x","path2"},
+            {"21531999","124.124.124.124","1","x","path2"},
+            {"21531999","124.124.124.124","1","x","path2"},
+            {"21531999","124.124.124.124","1","x","path2"},
+            {"21531999","124.124.124.124","1","x","path2"},
+            {"21531999","124.124.124.124","1","x","path2"},
+            {"21531999","124.124.124.124","1","x","path2"},
+            {"21531999","124.124.124.124","1","x","path2"},
+            {"21531999","124.124.124.124","1","x","path2"},
+            {"21531999","124.124.124.124","1","x","path2"},
+            {"21531999","124.124.124.124","1","x","path2"},
+            {"21531999","124.124.124.124","1","x","path2"},
+            {"21531999","124.124.124.124","1","x","path2"},
+            {"21531999","124.124.124.124","1","x","path2"},
+            {"21531999","124.124.124.124","1","x","path2"},
+            {"21531999","124.124.124.124","1","x","path2"},
+            {"21531999","124.124.124.124","1","x","path2"},
+            {"21531999","124.124.124.124","1","x","path2"},
+            {"21531999","124.124.124.124","1","x","path2"},
+            {"21531999","124.124.124.124","1","x","path2"},
+            {"21531999","124.124.124.124","1","x","path2"},
+            {"21531999","124.124.124.124","1","x","path2"},
+            {"21531999","124.124.124.124","1","x","path2"},
+            {"21531999","124.124.124.124","1","x","path2"},
+            {"21531999","124.124.124.124","1","x","path2"},
+            {"21531999","124.124.124.124","1","x","path2"},
+            {"21531999","124.124.124.124","1","x","path2"},
+        };
+        /** ラベルリスト*/
+        List<Label> labels = new List<Label>();
+        /** 列名*/
+        enum COL
+        {
+            INDEX,
+            UID,
+            IP,
+            COUNT,
+            ALERT,
+            PATH,
+            MAX
+        };
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        /** メンバーを更新する*/
+        void updateMember()
+        {
+            // 既存のテキストボックスを
+        }
+
+        // 各列の配置
+        AnchorStyles[] anc = {
+            AnchorStyles.Right,
+            AnchorStyles.Left,
+            AnchorStyles.Left,
+            AnchorStyles.Right,
+            AnchorStyles.Left,
+            AnchorStyles.Left
+                        };
+
+        /** メンバーを表示*/
+        void printMember(string [,] mems)
+        {
+            tableLayoutPanel1.Visible = false;
+            for (int i = 0,lbidx=(int)COL.MAX ; i < mems.GetLength(0); i++,lbidx+=(int)COL.MAX)
+            {
+                for (int j = 0; j < (int)COL.MAX; j++)
+                {
+                    // ラベルが不足していたら作成
+                    if (labels.Count < lbidx+(int)COL.MAX)
+                    {
+                        Label lbl = new Label();
+                        lbl.AutoSize = true;
+                        labels.Add(lbl);
+                        tableLayoutPanel1.Controls.Add(labels[labels.Count-1], j, i+1);
+                        tableLayoutPanel1.Controls[labels.Count-1].Anchor = anc[j];
+                    }
+
+                    // 文字列設定
+                    //// インデックス
+                    if (j==(int)COL.INDEX) {
+                        labels[lbidx].Text = (i+1).ToString();
+                    }
+                    else {
+                        //// その他の項目
+                        labels[lbidx+j].Text = mems[i,j-1];
+                    }
+                }
+            }
+            tableLayoutPanel1.Visible = true;
+        }
+
+        /** 起動時処理*/
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // タイトルの作成
+            tableLayoutPanel1.RowCount = 1;
+            string [] LABEL = {"通し番号", "学籍番号", "IP", "コピペ", "切断", "パス"};
+            for (int i=0 ; i<LABEL.Length ; i++) {
+                Label lbl = new Label();
+                lbl.AutoSize = true;
+                lbl.Text = LABEL[i];
+                labels.Add(lbl);
+                tableLayoutPanel1.Controls.Add(labels[labels.Count-1], i,0);
+                // パス以外
+                if (i < LABEL.Length - 1)
+                {
+                    //tableLayoutPanel1.ColumnStyles[i] = new ColumnStyle(SizeType.Absolute, lbl.Width * 2f);
+                    tableLayoutPanel1.ColumnStyles[i] = new ColumnStyle(SizeType.AutoSize);
+                }
+                else
+                {
+                    tableLayoutPanel1.ColumnStyles[i] = new ColumnStyle(SizeType.AutoSize);
+                }
+            }
+
+
+            // テスト表示
+            printMember(testData);
+        }
+
+        /** サイズ変更*/
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            tableLayoutPanel1.Width = ClientSize.Width;
         }
     }
 }

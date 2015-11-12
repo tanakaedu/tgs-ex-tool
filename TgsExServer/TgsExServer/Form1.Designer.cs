27f0472d84c88f821a5955614ea56530a712b2e2
YuTanaka <YRK00337@nifty.com>
2015-11-13 01:46:21 +0900

    UDPの送受信実装中

--- TgsExServer/TgsExServer/Form1.Designer.cs
+++ TgsExServer/TgsExServer/Form1.Designer.cs
@@ -28,7 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -49,11 +51,16 @@
             this.tableLayoutPanel1.Size = new System.Drawing.Size(326, 33);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
+            // timer1
+            // 
+            this.timer1.Interval = 3000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(732, 376);
+            this.ClientSize = new System.Drawing.Size(732, 562);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form1";
             this.Text = "試験サーバー";
@@ -68,6 +75,7 @@
         #endregion
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Timer timer1;
 
 
     }
--- TgsExServer/TgsExServer/Form1.cs
+++ TgsExServer/TgsExServer/Form1.cs
@@ -13,6 +13,10 @@ namespace TgsExServer
 {
     public partial class Form1 : Form
     {
+        // 送信回数
+        int Ser = 0;
+
+        // UDP
         IPAddress localAddress = IPAddress.Parse("127.0.0.1");
         int localPort = 60000;
         IPEndPoint localEP;
@@ -77,10 +81,7 @@ namespace TgsExServer
 
         public Form1()
         {
-            InitializeComponent();
-            // UDP起動
-            localEP = new IPEndPoint(localAddress, localPort);
-            udp = new UdpClient(localEP);
+            InitializeComponent();            
         }
 
         /** メンバーを更新する*/
@@ -116,19 +117,27 @@ namespace TgsExServer
                         tableLayoutPanel1.Controls.Add(labels[labels.Count-1], j, i+1);
                         tableLayoutPanel1.Controls[labels.Count-1].Anchor = anc[j];
                     }
-
-                    // 文字列設定
-                    //// インデックス
-                    if (j==(int)COL.INDEX) {
-                        labels[lbidx].Text = (i+1).ToString();
-                    }
-                    else {
-                        //// その他の項目
-                        labels[lbidx+j].Text = mems[i,j-1];
-                    }
                 }
+
+                // 行を出力
+                printRow(i, mems);
             }
             tableLayoutPanel1.Visible = true;
+
+        }
+
+        /** 行を表示*/
+        void printRow(int row, string[,] mems)
+        {
+            int lbidx = (row+1) * (int)COL.MAX;
+            // インデックス
+            labels[lbidx].Text = (row + 1).ToString();
+
+            // データ
+            for (int i = 0; i <mems.GetLength(1); i++)
+            {
+                labels[lbidx + i + 1].Text = mems[row, i];
+            }
         }
 
         /** 起動時処理*/
@@ -155,9 +164,36 @@ namespace TgsExServer
                 }
             }
 
-
             // テスト表示
             printMember(testData);
+
+            // UDP起動
+            localEP = new IPEndPoint(localAddress, localPort);
+            udp = new UdpClient(localEP);
+            
+            // 受信を開始
+            udp.BeginReceive(new AsyncCallback(ReceiveCallback), udp);
+
+            // ポーリングの開始
+            timer1.Enabled = true;
+        }
+
+        /** 受信コールバック*/
+        public void ReceiveCallback(IAsyncResult ar)
+        {
+            if (udp == null) return;
+            IPEndPoint ipAny = new IPEndPoint(IPAddress.Any, localPort);
+            Byte[] dat = udp.EndReceive(ar, ref ipAny);
+            string recv = System.Text.Encoding.GetEncoding("SHIFT-JIS").GetString(dat);
+
+            // 解体
+
+            // 設定
+            testData[0,5] = recv;
+            printRow(0, testData);
+
+            // 非同期開始
+            udp.BeginReceive(ReceiveCallback, udp);
         }
 
         /** サイズ変更*/
@@ -169,7 +205,24 @@ namespace TgsExServer
         /** 閉じる*/
         private void Form1_FormClosed(object sender, FormClosedEventArgs e)
         {
+            timer1.Enabled = false;
+
             udp.Close();
+            udp = null;
         }
+
+        /** ポーリング*/
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Ser++;
+            Text = "試験サーバー(" + Ser + ")";
+            Application.DoEvents();
+
+            // ブロードキャストで送信
+            Byte[] dat =
+                System.Text.Encoding.GetEncoding("SHIFT-JIS").GetBytes("call" + Ser);
+            udp.Send(dat, dat.GetLength(0));
+        }
+
     }
 }

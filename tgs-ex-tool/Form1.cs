826cc626643e9de4bf4e3f2815b5b79ff8e8f762
YuTanaka <YRK00337@nifty.com>
2015-11-14 14:18:10 +0900

    コピー時のアクティブウィンドウを記録 close #5

--- tgs-ex-tool/Form1.cs
+++ tgs-ex-tool/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿// http://dobon.net/vb/dotnet/graphics/screencapture.html
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -9,6 +10,7 @@ using System.Windows.Forms;
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
+using System.Runtime.InteropServices;
 
 namespace 試験登録
 {
@@ -31,9 +33,31 @@ namespace 試験登録
         /** コピーペースト回数*/
         int iCopyPasteCount = 0;
 
+        /** イベントハンドら*/
+        private MyClipboardViewer viewer;
+
         public Form1()
         {
+            viewer = new MyClipboardViewer(this);
+            // イベントハンドラの登録
+            viewer.ClipboardHandler += this.OnClipBoardChanged;
             InitializeComponent();
+        }
+
+        // クリップボードにテキストがコピーされると呼び出される
+        private void OnClipBoardChanged(object sender, ClipbardEventArgs args)
+        {
+            // 保存先確認
+            string path = getSavePath();
+            if (path.Length == 0) return;
+
+            // フォルダー作成
+            Directory.CreateDirectory(path);
+
+            // テキスト保存
+            File.WriteAllText(path + "\\copy.txt", args.Text);
+            // スクリーンショット保存
+            ScreenShot(path + "\\scr.png");
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -182,6 +206,99 @@ namespace 試験登録
                     + sUID);
                 client.Send(send, send.Length, remoteIP, SERVER_PORT);
             }
+        }
+
+        /** パス文字列を生成
+         * 受け取った保存先 > 学籍番号 > 日時
+         * @return 空=無効 / パス文字列
+         */
+        string getSavePath()
+        {
+            if (sSaveFolder.Length == 0)
+            {
+                return "";
+            }
+            string time = DateTime.Now.ToLongTimeString().Replace(':','-');
+            string path = sSaveFolder + sUID + "\\" + time;
+            for (int i = 0; i<10 ; i++)
+            {
+                if (!Directory.Exists(path+"-"+i)) {
+                    return path + "-" + i;
+                }
+            }
+
+            // 1秒で10個以上は無効
+            return "";
+        }
+
+
+        private const int SRCCOPY = 13369376;
+        private const int CAPTUREBLT = 1073741824;
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetDC(IntPtr hwnd);
+
+        [DllImport("gdi32.dll")]
+        private static extern int BitBlt(IntPtr hDestDC,
+            int x,
+            int y,
+            int nWidth,
+            int nHeight,
+            IntPtr hSrcDC,
+            int xSrc,
+            int ySrc,
+            int dwRop);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr ReleaseDC(IntPtr hwnd, IntPtr hdc);
+        [StructLayout(LayoutKind.Sequential)]
+        private struct RECT
+        {
+            public int left;
+            public int top;
+            public int right;
+            public int bottom;
+        }
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetWindowDC(IntPtr hwnd);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        private static extern int GetWindowRect(IntPtr hwnd,
+            ref  RECT lpRect);
+
+        /** 指定のパスにスクリーンショットを保存*/
+        void ScreenShot(string path)
+        {
+            //アクティブなウィンドウのデバイスコンテキストを取得
+            IntPtr hWnd = GetForegroundWindow();
+            IntPtr winDC = GetWindowDC(hWnd);
+            //ウィンドウの大きさを取得
+            RECT winRect = new RECT();
+            GetWindowRect(hWnd, ref winRect);
+            
+
+            //Bitmapの作成
+            Bitmap bmp = new Bitmap(winRect.right - winRect.left,
+                winRect.bottom - winRect.top);
+            //Graphicsの作成
+            Graphics g = Graphics.FromImage(bmp);
+
+            //Graphicsのデバイスコンテキストを取得
+            IntPtr hDC = g.GetHdc();
+            //Bitmapに画像をコピーする
+            BitBlt(hDC, 0, 0, bmp.Width, bmp.Height,
+                winDC, 0, 0, SRCCOPY);
+            //解放
+            g.ReleaseHdc(hDC);
+            g.Dispose();
+            ReleaseDC(hWnd, winDC);
+
+            // 保存
+            bmp.Save(path, System.Drawing.Imaging.ImageFormat.Png);
         }
     }
 }
---
+++ tgs-ex-tool/MyClipboardViewer.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+// http://www.atmarkit.co.jp/fdotnet/dotnettips/848cbviewer/cbviewer.html
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace 試験登録
+{
+    public class ClipbardEventArgs : EventArgs
+    {
+        private string text;
+
+        public string Text
+        {
+            get { return this.text; }
+        }
+
+        public ClipbardEventArgs(string str)
+        {
+            this.text = str;
+        }
+    }
+
+    public delegate void cbEventHandler(
+    object sender, ClipbardEventArgs ev);
+
+    [System.Security.Permissions.PermissionSet(
+        System.Security.Permissions.SecurityAction.Demand,
+        Name = "FullTrust")]
+    internal class MyClipboardViewer : NativeWindow
+    {
+        [DllImport("user32")]
+        public static extern IntPtr SetClipboardViewer(
+            IntPtr hWndNewViewer);
+        [DllImport("user32")]
+        public static extern bool ChangeClipboardChain(
+                IntPtr hWndRemove, IntPtr hWndNewNext);
+
+        [DllImport("user32")]
+        public extern static int SendMessage(
+                IntPtr hWnd, int Msg, IntPtr wParam, IntPtr lParam);
+
+        private const int WM_DRAWCLIPBOARD = 0x0308;
+        private const int WM_CHANGECBCHAIN = 0x030D;
+        private IntPtr nextHandle;
+
+        private Form parent;
+        public event cbEventHandler ClipboardHandler;
+
+        public MyClipboardViewer(Form f)
+        {
+            f.HandleCreated
+                        += new EventHandler(this.OnHandleCreated);
+            f.HandleDestroyed
+                        += new EventHandler(this.OnHandleDestroyed);
+            this.parent = f;
+        }
+
+        internal void OnHandleCreated(object sender, EventArgs e)
+        {
+            AssignHandle(((Form)sender).Handle);
+            // ビューアを登録
+            nextHandle = SetClipboardViewer(this.Handle);
+        }
+
+        internal void OnHandleDestroyed(object sender, EventArgs e)
+        {
+            // ビューアを解除
+            bool sts = ChangeClipboardChain(this.Handle, nextHandle);
+            ReleaseHandle();
+        }
+
+        protected override void WndProc(ref Message msg)
+        {
+            switch (msg.Msg)
+            {
+                case WM_DRAWCLIPBOARD:
+                    // データを列挙
+                    IDataObject data = Clipboard.GetDataObject();
+                    string[] formats = data.GetFormats();
+                    string res = "";
+                    bool isRecord = false;
+                    for (int i = 0; i < formats.Length; i++)
+                    {
+                        // EnhancedMetafileがあれば記録
+                        if (formats[i].IndexOf("EnhancedMetafile") != -1)
+                        {
+                            isRecord = true;
+                        }
+                        if (i > 0)
+                        {
+                            res += ",";
+                        }
+                        res += formats[i];
+                    }
+
+                    if (Clipboard.ContainsText())
+                    {
+                        // クリップボードの内容がテキストの場合のみ
+                        if (ClipboardHandler != null)
+                        {
+                            res += " : "+Clipboard.GetText()+"\r\n";
+                            isRecord = true;
+                        }
+                    }
+                    // クリップボードの内容を取得してハンドラを呼び出す
+                    if (isRecord)
+                    {
+                        ClipboardHandler(this,
+                            new ClipbardEventArgs(res));
+                    }
+
+
+                    if ((int)nextHandle != 0)
+                        SendMessage(
+                            nextHandle, msg.Msg, msg.WParam, msg.LParam);
+                    break;
+
+                // クリップボード・ビューア・チェーンが更新された
+                case WM_CHANGECBCHAIN:
+                    if (msg.WParam == nextHandle)
+                    {
+                        nextHandle = (IntPtr)msg.LParam;
+                    }
+                    else if ((int)nextHandle != 0)
+                        SendMessage(
+                            nextHandle, msg.Msg, msg.WParam, msg.LParam);
+                    break;
+            }
+            base.WndProc(ref msg);
+        }
+    }
+}
